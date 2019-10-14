97fa14117605d07bce810b014693a58c8045c75b
Yago Lopes <yagolopeslazaro@gmail.com>
2019-10-14 16:10:44 -0300

    configurando conexão para db remoto

--- backend/config/database.js
+++ backend/config/database.js
@@ -72,13 +72,15 @@ module.exports = {
   */
   pg: {
     client: 'pg',
-    connection: {
+    connection: Env.get('DB_URL'),
+    /* connection: {
       host: Env.get('DB_HOST', 'localhost'),
       port: Env.get('DB_PORT', ''),
       user: Env.get('DB_USER', 'root'),
       password: Env.get('DB_PASSWORD', ''),
       database: Env.get('DB_DATABASE', 'adonis'),
-    },
+      url:
+    },*/
     debug: Env.get('DB_DEBUG', false),
   },
 };
