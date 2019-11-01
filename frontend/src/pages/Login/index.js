35c1ad56be7e7216d58e72701ff0e2086b32a546
YagoLopes <yagolopeslazaro@gmail.com>
2019-11-01 11:52:37 -0300

    colocando o nome da pagina na class do compoente container, para diferenciar das outras paginas futuras

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import Logo from '../../assets/logoin.png';
-import './style.css';
+import React from "react";
+import Logo from "../../assets/logoin.png";
+import "./style.css";
 
 export default function Login() {
   return (
-    <div className="container">
-    <div className="img"> <img src={Logo} alt="logo"/></div>
-    <div className="form">
+    <div className="container-login">
+      <div className="img">
+        {" "}
+        <img src={Logo} alt="logo" />
+      </div>
+      <div className="form">
         <form>
-            <input type="text" placeholder="email@example.com"/>
-            <input type="password" placeholder="******"/>
-            <select name="eventos" id="1">
-                <option value=""></option>
-                <option value="evento teste1"></option>
-            </select>
-            <button type="submit">Entrar</button>
+          <input type="text" placeholder="email@example.com" />
+          <input type="password" placeholder="******" />
+          <select name="eventos" id="1">
+            <option value=""></option>
+            <option value="evento teste1"></option>
+          </select>
+          <button type="submit">Entrar</button>
         </form>
+      </div>
     </div>
-</div>
-
   );
 }
