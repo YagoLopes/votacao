dbc7bcbfb8aa7442af67efb2e844d58e8016770c
YagoLopes <yagolopeslazaro@gmail.com>
2019-11-01 11:51:39 -0300

    Removendo linhas comentadas

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,32 +1,32 @@
 import React from "react";
-import {BrowserRouter, Switch, Route, Redirect} from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Login from "./pages/Login";
-/*import Cadastro from "./pages/Cadastro";*/
+import Cadastro from "./pages/Cadastro";
 
 import { isAuthenticated } from "./services/auth";
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
-    <Route
-        {...rest}
-        render={props =>
-            isAuthenticated() ? (
-                <Component {...props} />
-            ) : (
-                <Redirect to={{ pathname: "/", state: { from: props.location}}} />
-            )
-        }
-    />
+  <Route
+    {...rest}
+    render={props =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+      )
+    }
+  />
 );
 
-const Routes = () =>(
-    <BrowserRouter>
+const Routes = () => (
+  <BrowserRouter>
     <Switch>
-        <Route exact path="/" component={Login}/>
-{/*<Route path="/cadUsuario" component={Cadastro} />*/}
-        <PrivateRoute path="/app" component={() => <h1>App</h1>} />
-        <Route path="*" component={() => <h1>Page not found</h1>}/>
+      <Route exact path="/" component={Login} />
+      <Route path="/cadastro/usuario" component={Cadastro} />
+      <PrivateRoute path="/app" component={() => <h1>App</h1>} />
+      <Route path="*" component={() => <h1>Page not found</h1>} />
     </Switch>
-    </BrowserRouter>
-)
+  </BrowserRouter>
+);
 
 export default Routes;
