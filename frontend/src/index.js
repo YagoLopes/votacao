f07e3fee94f37ec4e5ececf43ca0cbff28991588
YagoLopes <yagolopeslazaro@gmail.com>
2019-11-01 11:51:09 -0300

    importando o compoente Routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import Routes from "./routes";
+import "./reset.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-
-
+ReactDOM.render(<Routes />, document.getElementById("root"));
